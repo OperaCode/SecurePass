6ab2caaf6f02ef4a373d7e101b5900a66edc5ee2
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-07-07 17:39:55 +0100

    fixlandingview

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
      <Router>
       <Routes>
         {/* <Route path="/" element={<Landing />} /> */}
-        <Route path='/' element={Landing}/>
+        <Route path='/' element={<Landing/>}/>
         {/* <Route path="/home" element={<Home />} /> */}
         <Route path='/home' element={<Home/>}/>
       </Routes>
